6b793116c239bfa01a0261d2f8a23298a3d765b9
kevin <kevin.zhu@dinaping.com>
2016-03-07 23:20:11 +0800

    add some methond

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ app.use('/static', express.static('public'));
 
 
 
-
-
 app.get('/', function (req, res) {
 
-  res.send("<img id='img'" +
+  res.send("<img id='kobe_img'" +
                 "  src=\"/static/img/1.jpg\" \n" +
-                "  data-camanwidth=\"500\"\n" +
-                "  data-camanheight=\"500\"\n" +
+                "  width=\"500\"\n" +
+                "  height=\"500\"\n" +
                 ">"+
-              "<script type=\"text/javascript\" src=\"/static/javascript/test.js\"></script>");
+                "<canvas id='kobe_canvas' width=\"500\" height=\"400\"></canvas>"+
+                "<script type=\"text/javascript\" src=\"https://cdnjs.cloudflare.com/ajax/libs/camanjs/4.1.2/caman.full.min.js\"></script>"+
+              "<script type=\"text/javascript\" src=\"/static/javascript/test.js\"></script>"
+           );
 });
 
 var server = app.listen(3000, function () {
--- public/javascript/test.js
+++ public/javascript/test.js
@@ -1,1 +1,17 @@
-alert(1)
+
+var image = document.getElementById("kobe_img");
+var c = document.getElementById("kobe_canvas");
+var cxt = c.getContext("2d");
+cxt.drawImage(image,0,0);
+
+Caman("#kobe_canvas", image, function () {
+    this.brightness(-10);
+    this.newLayer(function () {
+        this.setBlendingMode("overlay");
+        this.opacity(10);
+        this.fillColor('#689900');
+        this.filter.brightness(15);
+        this.filter.contrast(10);
+    });
+    this.render();
+});
