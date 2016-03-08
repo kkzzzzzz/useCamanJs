2c9e088490f18535cb74be026cfa5880eef2fa9d
kevin <kevin.zhu@dinaping.com>
2016-03-08 16:25:56 +0800

    ignore

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -1,10 +1,11 @@
-
 var image = document.getElementById("kobe_img");
 var c = document.getElementById("kobe_canvas");
 var cxt = c.getContext("2d");
-cxt.drawImage(image,0,0);
+cxt.drawImage(image, 0, 0);
+
 
 Caman("#kobe_canvas", image, function () {
+
     this.brightness(-10);
     this.newLayer(function () {
         this.setBlendingMode("overlay");
