10ec32272b0b72bb77227cacf30b900c290561e8
kkzzzzz <zhukaidj@gmail.com>
2016-03-09 23:46:14 +0800

    why miss

--- app.js
+++ app.js
@@ -9,15 +9,22 @@ app.use('/static', express.static('public'));
 
 app.get('/', function (req, res) {
 
-  res.send("<img id='kobe_img'" +
-                "  src=\"/static/img/1.jpg\" \n" +
-                // "  width=\"500\"\n" +
-                // "  height=\"500\"\n" +
-                ">"+
-                "<canvas id='kobe_canvas' width=\"554\" height=\"667\"></canvas>"+
-                "<script type=\"text/javascript\" src=\"https://cdnjs.cloudflare.com/ajax/libs/camanjs/4.1.2/caman.full.min.js\"></script>"+
-              "<script type=\"text/javascript\" src=\"/static/javascript/test.js\"></script>"
-           );
+    res.send(
+        "<script src=\"//cdn.bootcss.com/jquery/2.2.1/jquery.js\"></script>"+
+        "<script type=\"text/javascript\" src=\"https://cdnjs.cloudflare.com/ajax/libs/camanjs/4.1.2/caman.full.min.js\"></script>" +
+        "<img id='kobe_img' " +
+        "  src=\"/static/img/1.jpg\" \n" +
+            // "  width=\"500\"\n" +
+            // "  height=\"500\"\n" +
+        ">" +
+        // "<canvas id='kobe_canvas' width=\"554\" height=\"667\"></canvas>" +
+        "<canvas id=\"canvas\" width=\"554\" height=\"667\"></canvas>"+
+        "<input type='button' id='vintagebtn' value='vintagebtn'></input>"+
+        "<button id =\"noisebtn\">Noise</button>"+
+        "<button id =\"tiltshiftbtn\">Tilt Shift</button>" +
+        "<script type=\"text/javascript\" src=\"/static/javascript/test.js\"></script>"
+    )
+    ;
 });
 
 var server = app.listen(3000, function () {
--- public/javascript/test.js
+++ public/javascript/test.js
@@ -1,11 +1,31 @@
 var image = document.getElementById("kobe_img");
-var c = document.getElementById("kobe_canvas");
-var cxt = c.getContext("2d");
-cxt.drawImage(image, 0, 0);
+// var c = document.getElementById("kobe_canvas");
+// var cxt = c.getContext("2d");
+// cxt.drawImage(image, 0, 0);
+
+// Caman("#kobe_canvas", image, function () {
+
+//     this.brightness(-10);
+//     this.newLayer(function () {
+//         this.setBlendingMode("overlay");
+//         this.opacity(100);
+//         this.fillColor('#689900');
+//         this.filter.brightness(15);
+//         this.filter.contrast(10);
+//     });
+//     this.render();
+// });
 
 
-Caman("#kobe_canvas", image, function () {
-
+var vintage = $('#vintagebtn');
+var noise = $('#noisebtn');
+var tiltshift = $('#tiltshiftbtn');
+  
+vintage.on('click', function(e) {
+  Caman('#canvas', image, function() {
+    var c = document.getElementById("canvas");
+    var cxt = c.getContext("2d");
+    cxt.drawImage(image, 0, 0);
     this.brightness(-10);
     this.newLayer(function () {
         this.setBlendingMode("overlay");
@@ -14,5 +34,23 @@ Caman("#kobe_canvas", image, function () {
         this.filter.brightness(15);
         this.filter.contrast(10);
     });
-    this.render();
+    this.render();   
+  });
 });
+  
+noise.on('click', function(e) {
+  Caman('#canvas', image, function() {
+    this.noise(10);
+    this.render();
+  });
+});
+  
+tiltshift.on('click', function(e) {
+  Caman('#canvas', image, function() {
+    this.tiltShift({
+      angle: 90,
+      focusWidth: 600
+    }).render();
+  });
+});
+
